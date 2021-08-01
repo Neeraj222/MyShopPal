33ec2d510917b8e60936897221d74accbb96dd44
Neeraj222 <49423198+Neeraj222@users.noreply.github.com>
2021-08-01 15:57:56 +1000

    Validating Register Class Entry
    - Validate Register Class Entries
    - Fixed Bug Error in Register Class
    - Commented out 2 seperate lines of code
    - Added dependancies for firebase auth
    -Enabled email sign in for user on firebase console

--- app/src/main/java/com/example/myshoppal/activities/LoginActivity.kt
+++ app/src/main/java/com/example/myshoppal/activities/LoginActivity.kt
@@ -24,7 +24,6 @@ class LoginActivity : AppCompatActivity() {
         setContentView(R.layout.activity_login)
 
         // This is used to hide the status bar and make the login screen as a full screen activity.
-        // It is deprecated in the API level 30. I will update you with the alternate solution soon.
         window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
@@ -36,5 +35,4 @@ class LoginActivity : AppCompatActivity() {
         }
 
     }
-
 }
--- app/src/main/java/com/example/myshoppal/activities/RegisterActivity.kt
+++ app/src/main/java/com/example/myshoppal/activities/RegisterActivity.kt
@@ -1,28 +1,105 @@
 package com.example.myshoppal.activities
 
-import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import android.view.WindowInsets
+import android.text.TextUtils
 import android.view.WindowManager
+import android.widget.TextView
 import com.example.myshoppal.R
 
 
-class RegisterActivity : AppCompatActivity() {
+@Suppress("DEPRECATION")
+class RegisterActivity : BaseActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_register)
 
-        @Suppress("DEPRECATION")
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            window.insetsController?.hide(WindowInsets.Type.statusBars())
-        } else {
-            window.setFlags(
-                WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN
-            )
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+//        setupActionBar()
+
+        val btnRegister = findViewById<TextView>(R.id.btn_register)
+        btnRegister.setOnClickListener {
+
+            validateRegisterDetails()
         }
+        // END
 
-
+        val btnLogin = findViewById<TextView>(R.id.tv_login)
+        btnLogin.setOnClickListener(){
+            val intent = Intent(this@RegisterActivity, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
+
+//    private fun setupActionBar() {
+//
+//        setSupportActionBar(toolbar_register_activity)
+//
+//        val actionBar = supportActionBar
+//        if (actionBar != null) {
+//            actionBar.setDisplayHomeAsUpEnabled(true)
+//            actionBar.setHomeAsUpIndicator(R.drawable.ic_black_color_back_24)
+//        }
+//
+//        toolbar_register_activity.setNavigationOnClickListener { onBackPressed() }
+//    }
+
+    private fun validateRegisterDetails(): Boolean {
+        val firstName = findViewById<TextView>(R.id.et_first_name)
+        val lastName = findViewById<TextView>(R.id.et_last_name)
+        val email = findViewById<TextView>(R.id.et_email)
+        val password = findViewById<TextView>(R.id.et_password)
+        val confirmPassword = findViewById<TextView>(R.id.et_confirm_password)
+        val termsAndCondition = findViewById<TextView>(R.id.cb_terms_and_condition)
+        return when {
+            TextUtils.isEmpty(firstName.text.toString()) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_first_name), true)
+                false
+            }
+
+            TextUtils.isEmpty(lastName.text.toString()) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_last_name), true)
+                false
+            }
+
+            TextUtils.isEmpty(email.text.toString()) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_email), true)
+                false
+            }
+
+            TextUtils.isEmpty(password.text.toString()) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_password), true)
+                false
+            }
+
+            TextUtils.isEmpty(confirmPassword.text.toString()) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_confirm_password), true)
+                false
+            }
+
+            password.text.toString() != confirmPassword.text.toString()
+                 -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_password_and_confirm_password_mismatch), true)
+                false
+            }
+//            !termsAndCondition.isChecked -> {
+//                showErrorSnackBar(resources.getString(R.string.err_msg_agree_terms_and_condition), true)
+//                false
+//            }
+
+            else -> {
+                showErrorSnackBar(resources.getString(R.string.registery_sucessfull), false)
+                true
+            }
+        }
+    }
+    // END
 }
