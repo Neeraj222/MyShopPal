d09c8d61f678611ef4cede7e1f560a770f597b60
Neeraj222 <49423198+Neeraj222@users.noreply.github.com>
2021-08-04 22:27:16 +1000

    Loading Profile Image And Verifying User Entries
    - Users are now able to pick there own image from the phone gallery
    - Verify User Entries in the user profile class

--- app/src/main/java/com/example/myshoppal/activities/UserProfileActivity.kt
+++ app/src/main/java/com/example/myshoppal/activities/UserProfileActivity.kt
@@ -1,8 +1,12 @@
 package com.example.myshoppal.activities
 
 import android.Manifest
+import android.app.Activity
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
@@ -10,12 +14,13 @@ import androidx.core.content.ContextCompat
 import com.example.myshoppal.R
 import com.example.myshoppal.models.User
 import com.example.myshoppal.utils.Constants
+import com.example.myshoppal.utils.GlideLoader
 import kotlinx.android.synthetic.main.activity_user_profile.*
+import java.io.IOException
 
 /**
  * A user profile screen.
  */
-// TODO Step 1: Implement the View.OnClickListener and replace the AppCompactActivity with BaseActivity.
 class UserProfileActivity : BaseActivity(), View.OnClickListener {
 
     /**
@@ -44,37 +49,33 @@ class UserProfileActivity : BaseActivity(), View.OnClickListener {
         et_email.isEnabled = false
         et_email.setText(userDetails.email)
 
-        // TODO Step 2: Assign the on click event to the user profile photo.
-        // START
         // Assign the on click event to the user profile photo.
         iv_user_photo.setOnClickListener(this@UserProfileActivity)
-        // END
+        btn_submit.setOnClickListener(this@UserProfileActivity)
     }
 
-    // TODO Step 5: Override the onClick function and assign the on click event to the user profile photo.
-    // START
     override fun onClick(v: View?) {
         if (v != null) {
             when (v.id) {
 
                 R.id.iv_user_photo -> {
 
-                    // Here we will check if the permission is already allowed or we need to request for it.
-                    // First of all we will check the READ_EXTERNAL_STORAGE permission and if it is not allowed we will request for the same.
                     if (ContextCompat.checkSelfPermission(
                             this,
                             Manifest.permission.READ_EXTERNAL_STORAGE
                         )
                         == PackageManager.PERMISSION_GRANTED
                     ) {
+                        // TODO Step 3: Remove the message and Call the image selection function here when the user already have the read storage permission.
+                        // START
+                        /*showErrorSnackBar("You already have the storage permission.",false)*/
 
-                        showErrorSnackBar("You already have the storage permission.", false)
+                        Constants.showImageChooser(this@UserProfileActivity)
+                        // END
                     } else {
-
                         /*Requests permissions to be granted to this application. These permissions
                          must be requested in your manifest, they should not be granted to your app,
                          and they should have protection level*/
-
                         ActivityCompat.requestPermissions(
                             this,
                             arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
@@ -82,13 +83,16 @@ class UserProfileActivity : BaseActivity(), View.OnClickListener {
                         )
                     }
                 }
+                R.id.btn_submit -> {
+                    if(validateUserProfileDetails()){
+                        showErrorSnackBar("Your details are valid. You can update them.",false)
+                    }
+                }
             }
         }
     }
     // END
 
-    // TODO Step 6: Override a function to identify the result of runtime permission after the user allows or deny the permission based on the unique code.
-    // START
     /**
      * This function will identify the result of runtime permission after the user allows or deny permission based on the unique code.
      *
@@ -105,8 +109,12 @@ class UserProfileActivity : BaseActivity(), View.OnClickListener {
         if (requestCode == Constants.READ_STORAGE_PERMISSION_CODE) {
             //If permission is granted
             if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // TODO Step 4: Remove the message and Call the image selection function here when the user grant the read storage permission.
+                // START
+                /*showErrorSnackBar("The storage permission is granted.",false)*/
 
-                showErrorSnackBar("The storage permission is granted.", false)
+                Constants.showImageChooser(this@UserProfileActivity)
+                // END
             } else {
                 //Displaying another toast if permission is not granted
                 Toast.makeText(
@@ -117,5 +125,67 @@ class UserProfileActivity : BaseActivity(), View.OnClickListener {
             }
         }
     }
-    // END
+
+    // TODO Step 5: Receive the result after selecting image from phone storage using the unique code that we have passed at a time of selection through intent.
+    // START
+    /**
+     * Receive the result from a previous call to
+     * {@link #startActivityForResult(Intent, int)}.  This follows the
+     * related Activity API as described there in
+     * {@link Activity#onActivityResult(int, int, Intent)}.
+     *
+     * @param requestCode The integer request code originally supplied to
+     *                    startActivityForResult(), allowing you to identify who this
+     *                    result came from.
+     * @param resultCode The integer result code returned by the child activity
+     *                   through its setResult().
+     * @param data An Intent, which can return result data to the caller
+     *               (various data can be attached to Intent "extras").
+     */
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == Constants.PICK_IMG_REQUEST_CODE) {
+                if (data != null) {
+                    try {
+                        // The uri of selected image from phone storage.
+                        val selectedImageFileUri = data.data!!
+
+//                        iv_user_photo.setImageURI(Uri.parse(selectedImageFileUri.toString()))
+                        GlideLoader(this).loadUserPicture(selectedImageFileUri, iv_user_photo)
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                        Toast.makeText(
+                            this@UserProfileActivity,
+                            resources.getString(R.string.image_selection_failed),
+                            Toast.LENGTH_SHORT
+                        )
+                            .show()
+                    }
+                }
+            }
+        } else if (resultCode == Activity.RESULT_CANCELED) {
+            // A log is printed when user close or cancel the image selection.
+            Log.e("Request Cancelled", "Image selection cancelled")
+        }
+    }
+    private fun validateUserProfileDetails(): Boolean {
+        return when {
+
+            // We have kept the user profile picture is optional.
+            // The FirstName, LastName, and Email Id are not editable when they come from the login screen.
+            // The Radio button for Gender always has the default selected value.
+
+            // Check if the mobile number is not empty as it is mandatory to enter.
+            TextUtils.isEmpty(et_mobile_number.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_mobile_number), true)
+                false
+            }
+            else -> {
+                true
+            }
+        }
+    }
+
+
 }
