91c863d59f14763f512178ab4e5dcec8f8a40100
Neeraj222 <49423198+Neeraj222@users.noreply.github.com>
2021-08-04 16:46:46 +1000

    User Permission
    - Added a feature where the app asks users permission to add image to the user profile page

--- app/src/main/java/com/example/myshoppal/activities/UserProfileActivity.kt
+++ app/src/main/java/com/example/myshoppal/activities/UserProfileActivity.kt
@@ -1,17 +1,22 @@
 package com.example.myshoppal.activities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.myshoppal.R
 import com.example.myshoppal.models.User
 import com.example.myshoppal.utils.Constants
-
 import kotlinx.android.synthetic.main.activity_user_profile.*
 
 /**
  * A user profile screen.
  */
-class UserProfileActivity : AppCompatActivity() {
+// TODO Step 1: Implement the View.OnClickListener and replace the AppCompactActivity with BaseActivity.
+class UserProfileActivity : BaseActivity(), View.OnClickListener {
 
     /**
      * This function is auto created by Android when the Activity Class is created.
@@ -22,19 +27,13 @@ class UserProfileActivity : AppCompatActivity() {
         // This is used to align the xml view to this class
         setContentView(R.layout.activity_user_profile)
 
-
-        // TODO Step 5: Retrieve the User details from intent extra.
-        // START
         // Create a instance of the User model class.
         var userDetails: User = User()
-        if(intent.hasExtra(Constants.EXTRA_USER_DETAILS)) {
+        if (intent.hasExtra(Constants.EXTRA_USER_DETAILS)) {
             // Get the user details from intent as a ParcelableExtra.
             userDetails = intent.getParcelableExtra(Constants.EXTRA_USER_DETAILS)!!
         }
-        // END
 
-        // TODO Step 6: After receiving the user details from intent set it to the UI.
-        // START
         // Here, the some of the edittext components are disabled because it is added at a time of Registration.
         et_first_name.isEnabled = false
         et_first_name.setText(userDetails.firstName)
@@ -44,6 +43,79 @@ class UserProfileActivity : AppCompatActivity() {
 
         et_email.isEnabled = false
         et_email.setText(userDetails.email)
+
+        // TODO Step 2: Assign the on click event to the user profile photo.
+        // START
+        // Assign the on click event to the user profile photo.
+        iv_user_photo.setOnClickListener(this@UserProfileActivity)
         // END
     }
+
+    // TODO Step 5: Override the onClick function and assign the on click event to the user profile photo.
+    // START
+    override fun onClick(v: View?) {
+        if (v != null) {
+            when (v.id) {
+
+                R.id.iv_user_photo -> {
+
+                    // Here we will check if the permission is already allowed or we need to request for it.
+                    // First of all we will check the READ_EXTERNAL_STORAGE permission and if it is not allowed we will request for the same.
+                    if (ContextCompat.checkSelfPermission(
+                            this,
+                            Manifest.permission.READ_EXTERNAL_STORAGE
+                        )
+                        == PackageManager.PERMISSION_GRANTED
+                    ) {
+
+                        showErrorSnackBar("You already have the storage permission.", false)
+                    } else {
+
+                        /*Requests permissions to be granted to this application. These permissions
+                         must be requested in your manifest, they should not be granted to your app,
+                         and they should have protection level*/
+
+                        ActivityCompat.requestPermissions(
+                            this,
+                            arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                            Constants.READ_STORAGE_PERMISSION_CODE
+                        )
+                    }
+                }
+            }
+        }
+    }
+    // END
+
+    // TODO Step 6: Override a function to identify the result of runtime permission after the user allows or deny the permission based on the unique code.
+    // START
+    /**
+     * This function will identify the result of runtime permission after the user allows or deny permission based on the unique code.
+     *
+     * @param requestCode
+     * @param permissions
+     * @param grantResults
+     */
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == Constants.READ_STORAGE_PERMISSION_CODE) {
+            //If permission is granted
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                showErrorSnackBar("The storage permission is granted.", false)
+            } else {
+                //Displaying another toast if permission is not granted
+                Toast.makeText(
+                    this,
+                    resources.getString(R.string.read_storage_permission_denied),
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+    }
+    // END
 }
--- app/src/main/java/com/example/myshoppal/utils/Constants.kt
+++ app/src/main/java/com/example/myshoppal/utils/Constants.kt
@@ -5,4 +5,5 @@ object Constants {
     const val MYSHOP_PREFERECNES: String = "MyShopPalPrefs"
     const val LOGGED_IN_USER: String = "logged_in_username"
     const val EXTRA_USER_DETAILS: String = "extra_user_details"
+    const val READ_STORAGE_PERMISSION_CODE = 2
 }
