5bc8a40719b5d5aa5d02dbbe2b210e5e8542e312
Neeraj222 <49423198+Neeraj222@users.noreply.github.com>
2021-08-15 19:18:09 +1000

    User now able to delete the product successfully

--- app/src/main/java/com/example/myshoppal/firestore/FireStoreClass.kt
+++ app/src/main/java/com/example/myshoppal/firestore/FireStoreClass.kt
@@ -283,5 +283,23 @@ class FirestoreClass {
             }
 
     }
+    fun deleteProduct(fragment: ProductsFragment, productId: String){
+        mFireStore.collection(Constants.PRODUCTS)
+            .document(productId)
+            .delete()
+            .addOnSuccessListener {
+                fragment.productDeleteSuccess()
+
+            }.addOnFailureListener{ e  ->
+                fragment.hideProgressDialog()
+                Log.e(
+                    fragment.requireActivity().javaClass.simpleName,"Error while deleting Product",
+                    e
+                )
+
+            }
+
+    }
+
 }
 
--- app/src/main/java/com/example/myshoppal/fragments/ProductsFragment.kt
+++ app/src/main/java/com/example/myshoppal/fragments/ProductsFragment.kt
@@ -1,5 +1,6 @@
 package com.example.myshoppal.fragments
 
+import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
 import android.view.*
@@ -84,14 +85,53 @@ class ProductsFragment : BaseFragment() {
     }
 
     fun deleteProduct(productID: String) {
-
-        // Here we will call the delete function of the FirestoreClass. But, for now lets display the Toast message and call this function from adapter class.
-
+        showAlertDialogToDeleteProduct(productID)
+    }
+    fun productDeleteSuccess(){
+        hideProgressDialog()
         Toast.makeText(
             requireActivity(),
-            "You can now delete the product. $productID",
-            Toast.LENGTH_SHORT
+            resources.getString(R.string.product_delete_success_message),
+            Toast.LENGTH_LONG
         ).show()
+        getProductListFromFireStore()
     }
+
+    private fun showAlertDialogToDeleteProduct(productID: String) {
+
+        val builder = AlertDialog.Builder(requireActivity())
+        //set title for alert dialog
+        builder.setTitle(resources.getString(R.string.delete_dialog_title))
+        //set message for alert dialog
+        builder.setMessage(resources.getString(R.string.delete_dialog_message))
+        builder.setIcon(android.R.drawable.ic_dialog_alert)
+
+        //performing positive action
+        builder.setPositiveButton(resources.getString(R.string.yes)) { dialogInterface, _ ->
+
+            // TODO Step 7: Call the function to delete the product from cloud firestore.
+            // START
+            // Show the progress dialog.
+            showProgressDialog(resources.getString(R.string.please_wait))
+
+            // Call the function of Firestore class.
+            FirestoreClass().deleteProduct(this@ProductsFragment, productID)
+            // END
+
+            dialogInterface.dismiss()
+        }
+
+        //performing negative action
+        builder.setNegativeButton(resources.getString(R.string.no)) { dialogInterface, _ ->
+
+            dialogInterface.dismiss()
+        }
+        // Create the AlertDialog
+        val alertDialog: AlertDialog = builder.create()
+        // Set other dialog properties
+        alertDialog.setCancelable(false)
+        alertDialog.show()
+    }
+    // END
     // END
 }
