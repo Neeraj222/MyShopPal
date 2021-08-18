f7120f4d21d103707f0b0d27dfbc5a09b1787002
Neeraj222 <49423198+Neeraj222@users.noreply.github.com>
2021-08-18 22:40:59 +1000

    Able to display user address in the app now

--- app/src/main/java/com/example/myshoppal/activities/AddressListActivity.kt
+++ app/src/main/java/com/example/myshoppal/activities/AddressListActivity.kt
@@ -3,8 +3,12 @@ package com.example.myshoppal.activities
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.myshoppal.R
+import com.example.myshoppal.adapters.AddressListAdapter
 import com.example.myshoppal.firestore.FirestoreClass
 import com.example.myshoppal.models.Address
 import kotlinx.android.synthetic.main.activity_address_list.*
@@ -38,15 +42,20 @@ class AddressListActivity : BaseActivity() {
     }
 
     fun successAddressListFromFirestore(addressList: ArrayList<Address>) {
-
         // Hide the progress dialog
         hideProgressDialog()
 
+        if(addressList.size > 0){
+            rv_address_list.visibility = View.VISIBLE
+            tv_no_address_found.visibility = View.GONE
+            rv_address_list.layoutManager = LinearLayoutManager(this@AddressListActivity)
+            rv_address_list.setHasFixedSize(true)
 
-        // Print all the list of addresses in the log with name.
-        for (i in addressList) {
-
-            Log.i("Name and Address", "${i.name} ::  ${i.address}")
+            val addressAdapter = AddressListAdapter(this, addressList)
+            rv_address_list.adapter = addressAdapter
+        }else{
+            rv_address_list.visibility = View.GONE
+            tv_no_address_found.visibility = View.VISIBLE
         }
     }
 
