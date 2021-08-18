b09ab7438bddf05217097fd388ac7c5789a4d3cd
Neeraj222 <49423198+Neeraj222@users.noreply.github.com>
2021-08-18 22:28:19 +1000

    Loading the Addresses from the Firestore Database to the app

--- app/src/main/java/com/example/myshoppal/activities/AddressListActivity.kt
+++ app/src/main/java/com/example/myshoppal/activities/AddressListActivity.kt
@@ -2,8 +2,11 @@ package com.example.myshoppal.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myshoppal.R
+import com.example.myshoppal.firestore.FirestoreClass
+import com.example.myshoppal.models.Address
 import kotlinx.android.synthetic.main.activity_address_list.*
 
 class AddressListActivity : BaseActivity() {
@@ -12,6 +15,7 @@ class AddressListActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_address_list)
         setupActionBar()
+        getAddressList()
 
         tv_add_address.setOnClickListener{
             val intent = Intent(this@AddressListActivity, AddEditAddressActivity::class.java)
@@ -32,5 +36,27 @@ class AddressListActivity : BaseActivity() {
 
         toolbar_address_list_activity.setNavigationOnClickListener { onBackPressed() }
     }
+
+    fun successAddressListFromFirestore(addressList: ArrayList<Address>) {
+
+        // Hide the progress dialog
+        hideProgressDialog()
+
+
+        // Print all the list of addresses in the log with name.
+        for (i in addressList) {
+
+            Log.i("Name and Address", "${i.name} ::  ${i.address}")
+        }
+    }
+
+    private fun getAddressList() {
+
+        showProgressDialog(resources.getString(R.string.please_wait))
+
+        FirestoreClass().getAddressesList(this@AddressListActivity)
+    }
+
+
 
 }
